909f3baa5d1bd27e5aab8d1f08ab706ed3fb9d78
Madison Ormsby <mormsby@usc.edu>
2024-01-29 23:18:29 -0800

    experiment done

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,4 +1,10 @@
 let drawing = false;
+let textColor;
+let lastColorChangeTime = 0;
+let textSizeValue = 3;
+let textColorStart;
+let textColorEnd;
+let meow = false;
 
 function setup() {
     canvasContainer = $("#canvas-container");
@@ -9,26 +15,64 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    background(183, 234, 247);
+    textColor = color(0);
+    textSize(textSizeValue);
+
 }
 
 function draw() {
-  if (drawing) {
-    // Draw a circle at the mouse position when the mouse is pressed and held
-    ellipse(mouseX, mouseY, 20, 20);
+    if (drawing) {
+      // Draw text at the mouse position when the mouse is pressed and held
+      fill(textColor);
+      textSize(textSizeValue);
+      text("owo?", mouseX, mouseY);
+      if (textSizeValue < 20) {
+        textSizeValue += 0.3;
+      }
+    }
+
+    if (drawing && keyIsPressed && key === '3' && millis() - lastColorChangeTime >= 30) {
+        // Change text color every second
+        textColor = color(random(255), random(255), random(255));
+        lastColorChangeTime = millis(); // Update the last color change time
+    }
+}
+  
+  function mousePressed() {
+    // Start drawing when the mouse is pressed
+    drawing = true;
+    if (meow == true){
+        textColor = color(random(255), random(255), random(255));
+    }
   }
-}
+  
+  function mouseReleased() {
+    // Stop drawing when the mouse is released
+    drawing = false;
+    textSizeValue = 3;
 
-function mousePressed() {
-  // Start drawing when the mouse is pressed
-  drawing = true;
-}
+  }
 
-function mouseReleased() {
-  // Stop drawing when the mouse is released
-  drawing = false;
-}
+  function keyPressed() {
+    // Change text color when any key is pressed
+    if (key === '1') {
+        textColor = color(0);
+      }
+    if (key === '2') {
+        textColor = color(random(255), random(255), random(255));
+      }
+
+    if (key == '4'){
+        background(183, 234, 247);
+    }
+
+    if (key == '5'){
+        meow = true;
+    }
+
+    if (key == '6'){
+        meow = false;
+    }
+  }
