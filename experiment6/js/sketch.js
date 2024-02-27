c10f00f4be6ce7a05d1d81f2786e9d8b561c9d3e
Madison Ormsby <mormsby@usc.edu>
2024-02-26 21:27:26 -0800

    GAH

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,33 +1,59 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// P_3_1_3_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+/**
+ * analysing and sorting the letters of a text
+ * changing the letters alpha value in relation to frequency
+ *
+ * MOUSE
+ * position x          : interpolate between normal text and sorted position
+ *
+ * KEYS
+ * a                   : toggle alpha mode
+ * s                   : save png
+ */
+'use strict';
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+var joinedText;
+var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ,.;!?1234567890 ';
+var counters = [];
 
-// Globals
-let myInstance;
-let canvasContainer;
+let yellow = (252, 186, 3);
+let black = (0, 0, 0);
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+var posX;
+var posY;
 
-    myMethod() {
-        // code to run when method is called
-    }
+let bee;
+let bg;
+
+var drawAlpha = true;
+
+function preload() {
+  joinedText = loadStrings('js/bee.txt');
+  bee = loadSound('js/bee.mp3')
+  bg = loadImage('js/bee.jpg')
 }
 
-// setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
+    let canvasContainer;
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
@@ -36,32 +62,86 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    // create 
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  noStroke();
+  textFont('monospace', 18);
+
+  joinedText = joinedText.join(' ');
+
+  // use the following command, to collect all characters in the text automatically
+  // alphabet = getUniqCharacters();
+
+  for (var i = 0; i < alphabet.length; i++) {
+    counters[i] = 0;
+  }
+
+  countCharacters();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(bg);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  posX = 20;
+  posY = 40;
+
+  // Inspired / used from https://editor.p5js.org/generative-design/sketches/P_3_1_3_01
+  for (var i = 0; i < joinedText.length; i++) {
+    // again, find the index of the current letter in the character set
+    var upperCaseChar = joinedText.charAt(i).toUpperCase();
+    var index = alphabet.indexOf(upperCaseChar);
+    if (index < 0) continue;
+    let randomValue = random(1);
+    let fillColor = lerp(yellow, black, randomValue);
+
+    if (drawAlpha) {
+
+      fill(fillColor, counters[index] * 3);
+    } else {
+      fill(0, 0, 0);
+    }
+
+    var sortY = index * 20 + 40;
+    var m = map(mouseY, 50, width - 50, 0, 1);
+    m = constrain(m, 0, 2);
+    var interY = lerp(posY, sortY, m);
+
+    text(joinedText.charAt(i), posX, interY);
+
+    posX += textWidth(joinedText.charAt(i));
+    if (posX >= width - 200 && upperCaseChar == ' ') {
+      posY += 30;
+      posX = 20;
+    }
+  }
+
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+    // Play or pause the song when the mouse is clicked
+    if (bee.isPlaying()) {
+      bee.pause();
+    } else {
+      bee.play();
+    }
+  }
+
+function countCharacters() {
+  for (var i = 0; i < joinedText.length; i++) {
+    // get one character from the text and turn it to uppercase
+    var c = joinedText.charAt(i);
+    var upperCaseChar = c.toUpperCase();
+    var index = alphabet.indexOf(upperCaseChar);
+    // increase the respective counter
+    if (index >= 0) counters[index]++;
+  }
 }
+
+function getUniqCharacters() {
+  var charsArray = joinedText.toUpperCase().split('');
+  var uniqCharsArray = charsArray.filter(function(char, index) {
+    return charsArray.indexOf(char) == index;
+  }).sort();
+  return uniqCharsArray.join('');
+}
+
---
+++ experiment7/js/sketch.js
@@ -0,0 +1,133 @@
+
+let apiEndpoint = 'https://api.openweathermap.org/data/2.5/weather?lat=36.9741&lon=122.03&appid=6845623ad53e049ee251c8bde57d4233';
+let data;
+
+let rectWidth = 500;
+let rectHeight = 100;
+let alphaValue = 0;
+let alphaIncrement = 5;
+
+let day;
+let night;
+
+let weather;
+let light = ['sunny']
+let dark = ['clouds', 'overcast', 'rain']
+let rain = ['rain', 'drizzling', 'showers']
+
+let raindrops = [];
+
+let bird;
+let wind;
+let water;
+
+
+
+function preload() {
+  day = loadImage('files/day.jpeg');
+  night = loadImage('files/night.jpeg');
+  water = loadSound('files/water.mp3');
+  wind = loadSound('files/wind.mp3');
+  bird = loadSound('files/bird.mp3');
+}
+
+function setup() {
+    // place our canvas, making it fit our container
+    let canvasContainer;
+    canvasContainer = $("#canvas-container");
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    canvas.parent("canvas-container");
+    // resize canvas is the page is resized
+    $(window).resize(function() {
+        console.log("Resizing...");
+        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+    });
+    // create 
+    loadJSON(apiEndpoint, gotData);
+
+    for (let i = 0; i < 100; i++) {
+      raindrops[i] = new Raindrop();
+    }
+
+}
+
+function draw() {
+  
+
+
+  if (data) {
+
+    weather = data.weather[0].description;
+    let isDark = dark.some(word => weather.includes(word));
+    let isLight = light.some(word => weather.includes(word));
+    let isRain = rain.some(word => weather.includes(word));
+
+
+    if (isDark == true){
+      background(night);
+      wind.play();
+      wind.setVolume(0.15);
+    }
+
+    if (isLight == true){
+      background(day);
+      bird.loop();
+      bird.setVolume(0.15);
+    }
+
+    if (isRain == true){
+      for (let i = 0; i < raindrops.length; i++) {
+        raindrops[i].fall();
+        raindrops[i].display();
+        water.loop();
+        water.setVolume(0.15);
+      }
+    }
+
+  }
+
+  // Display the data on the canvas
+  alphaValue += alphaIncrement;
+  if (alphaValue > 255 || alphaValue < 0) {
+      alphaIncrement *= -1; // Reverse increment direction when reaching boundaries
+  }
+  
+
+  // Draw the glowing white rectangle
+  fill(255, 255, 255, alphaValue);
+  rect(width / 2 - rectWidth / 2, height / 2 - rectHeight / 2, rectWidth, rectHeight);
+
+  textAlign(CENTER, CENTER);
+  fill(0);
+  textSize(18);
+  text(`Current Weather in Santa Cruz: ${weather}`, width / 2, height / 2);
+
+
+}
+
+function gotData(response) {
+  // Callback function to handle the API response
+  data = response;
+}
+
+class Raindrop {
+  constructor() {
+    this.x = random(width);
+    this.y = random(-500, -50);
+    this.length = random(5, 20);
+    this.speed = random(5, 15);
+  }
+
+  fall() {
+    this.y += this.speed;
+    if (this.y > height) {
+      this.y = random(-200, -100);
+      this.x = random(width);
+    }
+  }
+
+  display() {
+    stroke(0, 100, 255);
+    line(this.x, this.y, this.x, this.y + this.length);
+  }
+}
